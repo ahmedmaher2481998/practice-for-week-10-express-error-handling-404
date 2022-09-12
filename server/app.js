c2d052e386b1d10935a4e3991d4a6c0f1903216d
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-12 16:15:19 +0200

    solved

--- server/app.js
+++ server/app.js
@@ -1,9 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('GET / This is the root URL');
+app.get("/", (req, res) => {
+	res.send("GET / This is the root URL");
+});
+
+/*
+app.use((err, req, res, next) => {
+	const newErr = new Error("Sorry, the requested resource couldn't be found");
+	newErr.statusCode = 404;
+	next(newErr);
+});
+
+app.use((err, req, res, next) => {
+	console.log("object");
+	res.json({
+		message: err.message,
+		statusCode: err.statusCode || 500,
+	});
+});*/
+app.get("/*", (req, res) => {
+	throw new Error();
+});
+
+app.use((err, req, res, next) => {
+	err.statusCode = 404;
+	err.message = "Sorry, the requested resource couldn't be found";
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.status(err.statusCode || 500).json({
+		message: err.message,
+		statusCode: err.statusCode || 500,
+	});
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
